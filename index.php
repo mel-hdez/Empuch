724a5fddf14354eb94fc1ea91f062626dfbc2682
FerThosiba <f1nandocor@gmail.com>
2019-11-17 11:16:52 -0600

    Probando query para login

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <?php
 
-$server = "us-cdbr-iron-east-05.cleardb.net";
-$username = "b28c61d6887e8d";
-$password = "2be748af";
-$db = "heroku_71c0f8d00cb5a72";
+  $server = "us-cdbr-iron-east-05.cleardb.net";
+  $username = "b28c61d6887e8d";
+  $password = "2be748af";
+  $db = "heroku_71c0f8d00cb5a72";
 
-$conn = new mysqli($server, $username, $password, $db);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "ya ta";
+  $conn = new mysqli($server, $username, $password, $db);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
-$query = "SELECT * FROM users";
-$result = $conn->query($query);
+  $query = "SELECT * FROM users WHERE username='fer' AND password='123'";
+  $result = $conn->query($query);
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  echo "<table><tr><th>ID</th><th>Name</th></tr>";
-  while($row = $result->fetch_assoc()) {
-    echo "<tr><td>".$row["iduser"]."</td><td>".$row["username"]." ".$row["password"]."</td></tr>";
-}
-echo "</table>";
-} else {
-echo "0 results";
-}
-$conn->close();
+  if($data = mysqli_fetch_array($result)){
+    echo '1';
+  }
+  /*if ($result->num_rows > 0) {
+    // output data of each row
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    while($row = $result->fetch_assoc()) {
+      echo "<tr><td>".$row["iduser"]."</td><td>".$row["username"]." ".$row["password"]."</td></tr>";
+  }
+  echo "</table>";
+  } else {
+  echo "0 results";
+  }*/
+  $conn->close();
 ?>
--- login.php
+++ login.php
@@ -1,16 +1,19 @@
 <?php
-  $username = filter_input(INPUT_POST, "username");
-  $password = filter_input(INPUT_POST, "password");
+$username = filter_input(INPUT_POST, "username");
+$password = filter_input(INPUT_POST, "password");
 
-  $server = "us-cdbr-iron-east-05.cleardb.net";
-  $username = "b28c61d6887e8d";
-  $password = "2be748af";
-  $db = "heroku_71c0f8d00cb5a72";
-  
-  $conn = new mysqli($server, $username, $password, $db);
+$server = "us-cdbr-iron-east-05.cleardb.net";
+$username = "b28c61d6887e8d";
+$password = "2be748af";
+$db = "heroku_71c0f8d00cb5a72";
+
+$conn = new mysqli($server, $username, $password, $db);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
 
-  $query = "select * from `heroku_71c0f8d00cb5a72`.`users` where username = '".$username."' and password = '".$password."'";
+  $query = "select * from users where username = '".$username."' and password = '".$password."'";
   $result = $conn->query($query);
 
   if($data = mysqli_fetch_array($result)){
