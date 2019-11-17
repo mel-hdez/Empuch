cf064042b085dabd241f049c8fb65e7f7869d6e0
FerThosiba <f1nandocor@gmail.com>
2019-11-16 21:07:21 -0600

    pt3

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
 
-$url = parse_url(getenv("mysql://b28c61d6887e8d:2be748af@us-cdbr-iron-east-05.cleardb.net/heroku_71c0f8d00cb5a72?reconnect=true"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$server = "us-cdbr-iron-east-05.cleardb.net";
+$username = "b28c61d6887e8d";
+$password = "2be748af";
+$db = "heroku_71c0f8d00cb5a72";
 
 $conn = new mysqli($server, $username, $password, $db);
-echo "se conecto";
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
