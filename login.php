555a6554ea81e6a6f01b7cc3bef4480dbe501524
FerThosiba <f1nandocor@gmail.com>
2019-11-16 20:14:29 -0600

    Probando login 1

--- login.php
+++ login.php
@@ -2,9 +2,12 @@
   $username = filter_input(INPUT_POST, "username");
   $password = filter_input(INPUT_POST, "password");
 
-  if($username == "fer" && $password == "123"){
-    echo "1";
-  }else{
-    echo "2";
+  $mysqli = new mysqli("heroku_71c0f8d00cb5a72", "b28c61d6887e8d", "2be748af");
+
+  $result = mysqli_query($mysqli, 
+  "select * from `heroku_71c0f8d00cb5a72`.`users` where username = '".$username."' and password = '".$password."'");
+
+  if($data = mysqli_fetch_array($result)){
+    echo '1';
   }
 ?>
