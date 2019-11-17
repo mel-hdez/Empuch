9a6d331b9057d93bd6af5da903e10836384e6c44
FerThosiba <f1nandocor@gmail.com>
2019-11-16 21:29:22 -0600

    pt3

--- index.php
+++ index.php
@@ -6,13 +6,23 @@ $password = "2be748af";
 $db = "heroku_71c0f8d00cb5a72";
 
 $conn = new mysqli($server, $username, $password, $db);
-if (!$conn->connect_error) {
+if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "ya ta";
 
-$query = "select * from heroku_71c0f8d00cb5a72.users where username = 'fer' and password = '123'";
+$query = "SELECT * FROM users";
 $result = $conn->query($query);
-echo $result;
 
+if ($result->num_rows > 0) {
+  // output data of each row
+  echo "<table><tr><th>ID</th><th>Name</th></tr>";
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["iduser"]."</td><td>".$row["username"]." ".$row["password"]."</td></tr>";
+}
+echo "</table>";
+} else {
+echo "0 results";
+}
+$conn->close();
 ?>
