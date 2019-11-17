9a9b14cd77a7a0e66db635ede94e49a01d6fe45d
FerThosiba <f1nandocor@gmail.com>
2019-11-16 20:26:38 -0600

    asd

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+  $url = parse_url(getenv("mysql://b28c61d6887e8d:2be748af@us-cdbr-iron-east-05.cleardb.net/heroku_71c0f8d00cb5a72?reconnect=true"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
+
+  $conn = new mysqli($server, $username, $password, $db);
+
+
+?>
--- login.php
+++ login.php
@@ -2,11 +2,13 @@
   $username = filter_input(INPUT_POST, "username");
   $password = filter_input(INPUT_POST, "password");
 
+
   $mysqli = new mysqli("heroku_71c0f8d00cb5a72", "b28c61d6887e8d", "2be748af");
 
   $result = mysqli_query($mysqli, 
   "select * from `heroku_71c0f8d00cb5a72`.`users` where username = '".$username."' and password = '".$password."'");
 
+  echo $result;
   if($data = mysqli_fetch_array($result)){
     echo '1';
   }
