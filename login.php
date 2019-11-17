804a9b71142d29d7fda2f4bcea8cec37ed96184a
FerThosiba <f1nandocor@gmail.com>
2019-11-17 11:21:49 -0600

    Login finalizado

--- login.php
+++ login.php
@@ -1,23 +1,23 @@
 <?php
-$username = filter_input(INPUT_POST, "username");
-$password = filter_input(INPUT_POST, "password");
+  $username = filter_input(INPUT_POST, "username");
+  $password = filter_input(INPUT_POST, "password");
 
-$server = "us-cdbr-iron-east-05.cleardb.net";
-$username = "b28c61d6887e8d";
-$password = "2be748af";
-$db = "heroku_71c0f8d00cb5a72";
+  $server = "us-cdbr-iron-east-05.cleardb.net";
+  $dbuser = "b28c61d6887e8d";
+  $dbpass = "2be748af";
+  $db = "heroku_71c0f8d00cb5a72";
 
-$conn = new mysqli($server, $username, $password, $db);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+  $conn = new mysqli($server, $dbuser, $dbpass, $db);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
 
-  $query = "select * from users where username = '".$username."' and password = '".$password."'";
+  $query = "SELECT * FROM users WHERE username = '".$username."' AND password = '".$password."'";
   $result = $conn->query($query);
 
   if($data = mysqli_fetch_array($result)){
     echo '1';
   }
-  $conn->close();
+$conn->close();
 ?>
