d28ba92915acfbcf40fe319f078bc7e41e1318f8
Lechuga <f1nandocor@gmail.com>
2019-12-12 09:04:39 -0600

    a trab

--- user.php
+++ user.php
@@ -5,18 +5,25 @@ $menu = $_REQUEST['opcion'];
 $id = $_REQUEST['id'];
 
 if($menu == "mascota"){
-  $petQuery = "SELECT * FROM mascota WHERE id_usuario = '$id'";
-  $petResult = mysqli_query($conn, $petQuery);
-  $petData = array();
-  foreach($petResult as $row){
-    $petData[] = $row;
+  $mascotaQuery = "SELECT * FROM mascota WHERE id_usuario = '$id'";
+  $mascotaResultado = mysqli_query($conn, $mascotaQuery);
+  $mascotaInfo = array();
+  foreach($mascotaResultado as $row){
+    $mascotaInfo[] = $row;
   }
   mysqli_close($conn);
-  echo json_encode($petData);
+  echo json_encode($mascotaInfo);
 }
 
 if($menu == "citas"){
-  
+  $citaQuery = "SELECT * FROM cita WHERE id_usuario = '$id'";
+  $citaResultado = mysqli_query($conn, $citaQuery);
+  $citaInfo = array();  
+  foreach($citaResultado as $row){
+    $citaInfo[] = $row;
+  }
+  mysqli_close($conn);
+  echo json_encode($citaInfo);
 }
 
 if($menu == "historial"){
@@ -24,6 +31,13 @@ if($menu == "historial"){
 }
 
 if($menu == "usuario"){
-
+  $usuarioQuery = "SELECT * FROM usuario WHERE id_usuario = '$id'";
+  $usuarioResultado = mysqli_query($conn, $usuarioQuery);
+  $usuarioInfo = array();
+  foreach($usuarioInfo as $row){
+    $usuarioInfo[] = $row;
+  }
+  mysqli_close($conn);
+  echo json_encode($usuarioInfo);
 }
 ?>
